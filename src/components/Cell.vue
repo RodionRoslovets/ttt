fb20def46ae8d6b55e2a83e21c33575431c8fd31
Rodion <rodion18570@gmail.com>
2021-02-13 12:09:11 +0300

    create base

---
+++ src/components/Cell.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="cell"
+    @click="makeMove('player', $event)"></div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+
+        }
+    },
+    methods:{
+        //ход игрока
+        makeMove(player, e){
+            if(player === 'player' && e.target.innerHTML === "" ){
+                e.target.innerHTML = "X"
+
+                this.$emit('checkEndGame')
+            } 
+        }
+    }
+}
+</script>
+
+<style>
+    .cell{
+        width: 100px;
+        height: 100px;
+        border:1px solid black;
+        box-sizing: border-box;
+        text-align: center;
+        font-size: 24px;
+        line-height: 100px;
+    }
+    .cell:hover{
+        background:#ececec;
+        cursor:pointer;
+    }
+</style>
---
+++ src/components/MainLayout.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container">
+      <cell v-for="n in 9" :key="n" @checkEndGame="checkEndGame">
+
+      </cell>
+  </div>
+</template>
+
+<script>
+import Cell from './Cell'
+export default {
+    components:{
+        Cell
+    },
+    data(){
+        return {
+            //выйгрышные комбинации
+            winCombinations:[
+                [1,2,3],
+                [4,5,6],
+                [7,8,9],
+                [1,4,7],
+                [2,5,8],
+                [3,6,9],
+                [1,5,9],
+                [3,5,7]
+            ]
+        }
+    },
+    methods:{
+        clearField(){
+            this.$children.forEach(cell => cell.$el.innerHTML = '')
+        },
+        checkEndGame(){
+            let player = [],
+                ai = [],
+                isGameEnd = false,
+                isPlayerWin = false,
+                isAiWin = false
+
+            this.$children.forEach((cell,index)=>{
+                if(cell.$el.innerHTML === 'X'){
+                    player.push(index + 1)
+                } else if(cell.$el.innerHTML === 'O') {
+                    ai.push(index + 1)
+                }
+            })
+
+            this.winCombinations.forEach(combination => {
+                if(player.filter((elem)=>{return combination.indexOf(elem) !== -1}).length == 3){
+                    isPlayerWin = true
+                }
+            })
+
+            this.winCombinations.forEach(combination => {
+                if(ai.filter((elem)=>{return combination.indexOf(elem) !== -1}).length == 3){
+                    isAiWin = true
+                }
+            })
+
+            if(isPlayerWin || isAiWin) {
+                isGameEnd = true
+            }
+
+            if(isGameEnd || this.$children.filter(cell => cell.$el.innerHTML === '').length === 0){
+                this.clearField()
+            } else {
+                this.aiMove()
+            }
+        },
+        //ход ии
+        aiMove(){
+            let randIndex = Math.floor(Math.random() * 9)
+
+            if(this.$children[randIndex].$el.innerHTML === ''){
+                this.$children[randIndex].$el.innerHTML = 'O'
+            } else {
+                this.aiMove()
+            }
+
+            // this.checkEndGame()
+        }
+    }
+}
+</script>
+
+<style>
+
+.container{
+    width: 300px;
+    height: 300px;
+    display:flex;
+    flex-wrap: wrap;
+}
+
+</style>
